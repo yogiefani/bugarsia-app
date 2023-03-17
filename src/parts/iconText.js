d8a3dfbe4b17e7d39bea218f1e2290a01e4d012b
Yogi Efani Yancandra <106648832+yogiefani@users.noreply.github.com>
2023-03-17 13:14:03 +0700

    melakukan banyak perubahan

---
+++ src/parts/iconText.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from 'elements/Button'
+
+export default function IconText() {
+  return (
+    <Button className='brand-text-icon' href="" type='link'>
+        Bugar<span className='text-gray-800'>sia.</span>
+    </Button>
+  )
+}
