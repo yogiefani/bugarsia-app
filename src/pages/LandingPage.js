cd6853131791f944b1ffd5523494fe11680c2645
Yogi Efani Yancandra <106648832+yogiefani@users.noreply.github.com>
2023-03-31 22:24:39 +0700

    fix misstyped class + complete component parts/footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Header from 'parts/Header';
-import landingPage from 'json/landingPage.json'
-import Hero from 'parts/Hero';
-import MostPicked from 'parts/MostPicked';
-import Categories from 'parts/Categories';
+import Header from "parts/Header";
+import landingPage from "json/landingPage.json";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 export default class LandingPage extends Component {
-    constructor(props){
-        super(props);
-        this.refMostPicked = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
-      <Header {...this.props}></Header>
-      <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
-      <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
-      <Categories data={landingPage.categories} />
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
